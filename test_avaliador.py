3ded7c21344bb7d304797e90d43462cf9127bddd
Vitor <vitor.condexd@gmail.com>
2019-12-12 16:46:48 -0300

    Aula 01.4
    Utilizando unittest ctrl shifit T criando testes.

---
+++ test_avaliador.py
@@ -0,0 +1,24 @@
+from unittest import TestCase
+from dominio import Usuario, Lance, Leilao, Avaliador
+
+class TestAvaliador(TestCase):
+    def test_avalia(self):
+        gui = Usuario('Gui')
+        yuri = Usuario('Yuri')
+
+        lance_do_yuri = Lance(yuri, 100.0)
+        lance_do_gui = Lance(gui, 150.0)
+
+        leilao = Leilao('Celular')
+
+        leilao.lances.append(lance_do_gui)
+        leilao.lances.append(lance_do_yuri)
+
+        avaliador = Avaliador()
+        avaliador.avalia(leilao)
+
+        menor_valor_esperado = 100.0
+        self.assertEqual(menor_valor_esperado, avaliador.menor_lance)
+
+        maior_valor_esperado = 150.0
+        self.assertEqual(maior_valor_esperado, avaliador.maior_lance)
