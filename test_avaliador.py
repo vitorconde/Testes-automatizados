699e7cdbeb4fed5dcf5051c0fc945df1ff781ead
Vitor <vitor.condexd@gmail.com>
2019-12-13 17:33:19 -0300

    Aula 03.1
    Encapsulamento

--- test_avaliador.py
+++ test_avaliador.py
@@ -14,8 +14,8 @@ class TestAvaliador(TestCase):
     def test_deve_retornar_o_maior_e_o_menor_valor_de_um_lance_quando_adicionados_em_ordem_crescente(self):
         yuri = Usuario('Yuri')
         lance_do_yuri = Lance(yuri, 100.0)
-        self.leilao.lances.append(lance_do_yuri)
-        self.leilao.lances.append(self.lance_do_gui)
+        self.leilao.propoe(lance_do_yuri)
+        self.leilao.propoe(self.lance_do_gui)
 
         avaliador = Avaliador()
         avaliador.avalia(self.leilao)
@@ -31,8 +31,8 @@ class TestAvaliador(TestCase):
         yuri = Usuario('Yuri')
         self.lance_do_yuri = Lance(yuri, 100.0)
 
-        self.leilao.lances.append(self.lance_do_gui)
-        self.leilao.lances.append(self.lance_do_yuri)
+        self.leilao.propoe(self.lance_do_gui)
+        self.leilao.propoe(self.lance_do_yuri)
 
         avaliador = Avaliador()
         avaliador.avalia(self.leilao)
@@ -45,7 +45,7 @@ class TestAvaliador(TestCase):
 
 
     def test_deve_retornar_o_mesmo_valor_para_o_maior_e_menor_lance_quando_leilao_tiver_um_lance(self):
-        self.leilao.lances.append(self.lance_do_gui)
+        self.leilao.propoe(self.lance_do_gui)
 
         avaliador = Avaliador()
         avaliador.avalia(self.leilao)
@@ -60,9 +60,9 @@ class TestAvaliador(TestCase):
         vini = Usuario('Vini')
         lance_do_vini = Lance(vini, 200.0)
 
-        self.leilao.lances.append(self.lance_do_yuri)
-        self.leilao.lances.append(self.lance_do_gui)
-        self.leilao.lances.append(lance_do_vini)
+        self.leilao.propoe(self.lance_do_yuri)
+        self.leilao.propoe(self.lance_do_gui)
+        self.leilao.propoe(lance_do_vini)
 
         avaliador = Avaliador()
         avaliador.avalia(self.leilao)
