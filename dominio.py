9af290ce54592549d847201114f2f9e2e82b9717
Vitor <vitor.condexd@gmail.com>
2019-12-18 15:53:58 -0300

    Aula 06.1
    Isolando condições dos Ifs.

--- dominio.py
+++ dominio.py
@@ -1,5 +1,3 @@
-import sys
-
 class Usuario:
 
     def __init__(self, nome, carteira):
@@ -7,7 +5,7 @@ class Usuario:
         self.__carteira = carteira
 
     def propoe_lance(self,leilao, valor):
-        if valor > self.__carteira:
+        if self._valor_e_valido(valor):
             raise ValueError('Não pode propor um lance com valor maior que o valor da carteira')
         lance = Lance(self, valor)
         leilao.propoe(lance)
@@ -22,6 +20,10 @@ class Usuario:
     def carteira(self):
         return self.__carteira
 
+    def _valor_e_valido(self, valor):
+        return valor > self.__carteira
+
+
 class Lance:
 
     def __init__(self, usuario, valor):
@@ -34,15 +36,15 @@ class Leilao:
     def __init__(self, descricao):
         self.descricao = descricao
         self.__lances = []
-        self.maior_lance = sys.float_info.min
-        self.menor_lance = sys.float_info.max
+        self.maior_lance = 0.0
+        self.menor_lance = 0.0
 
     def propoe(self, lance: Lance):
-        if not len(self.__lances) or self.__lances[-1].usuario != lance.usuario and lance.valor > self.__lances[-1].valor:
-            if lance.valor > self.maior_lance:
-                self.maior_lance = lance.valor
-            if lance.valor < self.menor_lance:
+        if self._lance_e_valido(lance):
+            if not self._tem_lances():
                 self.menor_lance = lance.valor
+
+            self.maior_lance = lance.valor
 
             self.__lances.append(lance)
         else:
@@ -51,3 +53,16 @@ class Leilao:
     @property
     def lances(self):
         return self.__lances[:]
+
+    def _tem_lances(self):
+        return self.__lances
+
+    def _usuarios_diferentes(self, lance):
+        return self.__lances[-1].usuario != lance.usuario
+
+    def _valor_maior_que_lance_anterior(self, lance):
+        return lance.valor > self.__lances[-1].valor
+
+    def _lance_e_valido(self, lance):
+        return not self._tem_lances() or (self._usuarios_diferentes(lance) and
+                                          self._valor_maior_que_lance_anterior(lance))
