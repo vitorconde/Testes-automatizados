8141ee5836a9cc399b577441a835cdea32873d85
Vitor <vitor.condexd@gmail.com>
2019-12-18 11:56:05 -0300

    Aula 05.4
    Criando metodo de construtor de objetos necessários para utilizar no pytest @pytest.fixture

--- dominio.py
+++ dominio.py
@@ -7,6 +7,8 @@ class Usuario:
         self.__carteira = carteira
 
     def propoe_lance(self,leilao, valor):
+        if valor > self.__carteira:
+            raise ValueError('Não pode propor um lance com valor maior que o valor da carteira')
         lance = Lance(self, valor)
         leilao.propoe(lance)
 
